d2d9509118f39ac9233a39a18a97164320c4d694
kohenil <kohenil97@gmail.com>
2023-11-26 10:27:03 +0900

    add code for collect eatch commpanny throw point

--- back/echCom/actResult.py
+++ back/echCom/actResult.py
@@ -21,3 +21,14 @@ def mk_qrUrl(dataDic):
 
 
 if __name__ == '__main__':
+    aa = {
+        'serviceKey': 'HTUHdPFs%2Bqea3owdV7jymHuLsj5S0zQ3SOOzGSzH%2BwGebg3xSXgK2igtN14S2BMYeTMA6spbad3PQ5Ia8ZEtGg%3D%3D',
+        'numOfRows': 999,
+        'pageNo': 1,
+        'bidNtceNo': '20231120308',
+        'bidClsfcNo': 0,
+        'rbidNo': 0,
+        'type': 'json'
+    }
+    a = mk_qrUrl(aa)
+    print(a)
